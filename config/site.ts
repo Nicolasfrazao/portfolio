e8cd87e26ff014ae0feaebde922023b48d7875cf
Nick Frazao <nicolas_frazao@hotmail.com>
2024-06-16 17:28:08 -0300

    First day of my portfolio

---
+++ config/site.ts
@@ -0,0 +1,69 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "Nicolas Portfolio",
+  description: "Nicolas Frazao portfolio, created to showcase my code skills, build with nextJS.",
+  
+  navItems: [
+    {
+      label: "Home",
+      href: "/",
+    },
+    {
+      label: "Docs",
+      href: "/docs",
+    },
+    {
+      label: "Pricing",
+      href: "/pricing",
+    },
+    {
+      label: "Blog",
+      href: "/blog",
+    },
+    {
+      label: "About",
+      href: "/about",
+    },
+  ],
+  navMenuItems: [
+    {
+      label: "Profile",
+      href: "/profile",
+    },
+    {
+      label: "Dashboard",
+      href: "/dashboard",
+    },
+    {
+      label: "Projects",
+      href: "/projects",
+    },
+    {
+      label: "Team",
+      href: "/team",
+    },
+    {
+      label: "Calendar",
+      href: "/calendar",
+    },
+    {
+      label: "Settings",
+      href: "/settings",
+    },
+    {
+      label: "Help & Feedback",
+      href: "/help-feedback",
+    },
+    {
+      label: "Logout",
+      href: "/logout",
+    },
+  ],
+  links: {
+    github: "https://github.com/Nicolasfrazao",
+    linkedin: "https://www.linkedin.com/in/nicolas-fraz%C3%A3o-b6591225b/",
+    discord: "https://discord.gg/rtkH6d22",
+    sponsor: "patreon.com/NFDev",
+  },
+};
