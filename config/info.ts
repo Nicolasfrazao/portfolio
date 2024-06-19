7ff5f87cf9aa8c9cf40479840b00d9d7ff8a5668
Nick Frazao <nicolas_frazao@hotmail.com>
2024-06-19 17:45:26 -0300

    "Removed skills config file and added new projects to projects config file"

---
+++ config/info.ts
@@ -0,0 +1,112 @@
+export const skills = [
+  "MongoDB",
+  "Mongoose",
+  "Express",
+  "React",
+  "Node.js",
+  "JavaScript",
+  "TypeScript",
+  "HTML",
+  "CSS",
+  "Git",
+  "GitHub",
+  "NoSQL",
+  "SQL",
+  "Tailwind CSS",
+  "Docker",
+  "Kubernetes",
+  "AWS",
+  "Computer Science",
+  "Data Structures",
+  "Algorithms",
+  "Object Oriented Programming",
+  "Software Engineering",
+  "Responsive Design",
+  "Business Plan",
+  "Business Management",
+];
+
+export const softSkills = [
+  "Teamwork",
+  "Communication",
+  "Problem Solving",
+  "Adaptability",
+  "Time Management",
+  "Critical Thinking",
+  "Leadership",
+  "Emotional Intelligence",
+  "Flexibility",
+  "Work ethic",
+  "Collaboration",
+  "Creative Thinking",
+  "Decision Making",
+  "Self-Starter",
+  "Self-Awareness",
+  "Collaboration",
+  "Adaptability",
+  "Self-Motivation",
+  "Self-Awareness",
+  "Critical Thinking",
+];
+
+export const publicContributions = [
+  {
+    title: "NextJS",
+    description: "I helped to solve problems related with the framework"
+  },
+  {
+    title: "Tailwind CSS",
+    description: "I helped to solve problems related with the framework"
+  },
+  {
+    title: "TypeScript",
+    description: "I helped to solve problems related with the language"
+  },
+  {
+    title: "Vercel",
+    description: "I helped to solve problems related with the platform"
+  },
+  {
+    title: "React",
+    description: "I helped to solve problems related with the framework"
+  },
+  {
+    title: "VSCode",
+    description: "Test"
+  },
+]
+
+export const aboutMe = {
+  name: "Nicolas Matheus Macedo Frazao",
+  languages: ["English", "Portuguese", "Spanish"],
+  hobbies: [
+    "Music",
+    "Travel",
+    "Learn",
+    "Cooking",
+    "Sports",
+    "Movies",
+    "Gaming",
+    "Driving",
+  ],
+};
+
+export const certifications = [
+  "Computer Science",
+  "Java",
+  "JavaScript",
+  "MongoDB",
+  "Node.js",
+  "React",
+  "SQL",
+  "TypeScript",
+  "HTML",
+  "CSS",
+  "Git",
+  "GitHub",
+  "NoSQL",
+  "Tailwind CSS",
+  "Docker",
+  "Kubernetes",
+  "AWS",
+];
--- config/projects.ts
+++ config/projects.ts
@@ -39,4 +39,83 @@ export const ProjectData = [
       "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
     tags: ["NextJS", "TailwindCSS", "Docker", "GitHub Actions"],
   },
-]
+  {
+    title: "Music App",
+    description: "Music app using the Spotify API",
+    href: "https://github.com/NicolasFrazao/music-app",
+    image:
+      "https://raw.githubusercontent.com/NicolasFrazao/music-app/main/public/logo.png",
+    tags: ["React native", "Tailwind CSS", "Docker", "GitHub Actions"],
+  },
+  {
+    title: "Portfolio",
+    description: "This portfolio",
+    href: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+    image:
+      "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+    tags: ["Next.js", "Tailwind CSS", "TypeScript", "Vercel"],
+  },
+  {
+    title: "E-Commerce",
+    description: "E-Commerce website using Next.js and TypeScript",
+    href: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+    image:
+      "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+    tags: ["Next.js", "Tailwind CSS", "TypeScript", "Vercel"],
+  },
+  {
+    title: "PC parts",
+    description: "App to find and build and your dream pc for the best price",
+    href: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+    image:
+      "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+    tags: ["React", "TypeScript", "Vercel"],
+  },
+  {
+    title: "RepairNow",
+    description: "App for share guides and items to repair eletronics",
+    href: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+    image:
+      "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+    tags: ["React", "TypeScript", "Vercel"],
+  },
+  {
+    title: "Socialize",
+    description: "Social Media app to share pictures and videos",
+    href: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+    image:
+      "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+    tags: ["Next.js", "Tailwind CSS", "TypeScript", "Vercel"],
+  },
+  {
+    title: "Chat App",
+    description: "Chat app using React and TypeScript",
+    href: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+    image:
+      "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+    tags: ["React", "TypeScript", "Vercel"],
+  },
+  {
+    title: "WECnow",
+    description:
+      "App to watch the world endurance championship and keep informed of your favorite team",
+    href: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+    image:
+      "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+    tags: ["React", "TypeScript", "Vercel"],
+  },
+  {
+    title: "NFBank",
+    description: "Bank app",
+    href: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+    image:
+      "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+  },
+  {
+    title: "Mods Base",
+    description: "App to share and download mods for your favorite games",
+    href: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+    image:
+      "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+  },
+];
