9a1076beef4d665aae7d8b409c76d64b9849450d
Nick Frazao <nicolas_frazao@hotmail.com>
2024-06-20 16:52:12 -0300

    "Refactor Home page and Navbar components: updated text and layout in Home page, removed search input and adjusted icons in Navbar"

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,27 @@
 export default function Home() {
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-      <div className="inline-block max-w-lg text-center justify-center">
+      <div className="inline-block max-w-4xl text-center justify-center p-3">
         <h1 className="text-4xl font-serif">About Me:</h1>
-        <p
-          className="text-xl text-center "
-        >
+        <p className=" text-base">
           I am a dedicated Full Stack Engineer with a strong passion for
           problem-solving and developing innovative solutions. With expertise in
           the MERN stack (MongoDB, Express.js, React, Node.js) and Next.js, I
           bring a robust skill set to design, develop, and maintain scalable web
           applications. I am committed to continuous learning and skill
           enhancement, always striving to stay updated with the latest industry
-          trends and best practices. My goal is to leverage my technical
-          proficiency and creativity to deliver high-quality, user-centric
-          products. I am enthusiastic about new technologies and enjoy exploring
-          and implementing cutting-edge solutions. Beyond my professional
-          interests, I am passionate about fitness, sports, traveling, and
-          self-improvement. I thrive on pushing my limits and embracing new
-          challenges, both in my career and personal life.
+          trends and best practices.
+        </p>
+        <p className=" text-base mt-4">
+          My goal is to leverage my technical proficiency and creativity to
+          deliver high-quality, user-centric products. I am enthusiastic about
+          new technologies and enjoy exploring and implementing cutting-edge
+          solutions.
+        </p>
+        <p className=" text-base mt-4">
+          Beyond my professional interests, I am passionate about fitness,
+          sports, traveling, and self-improvement. I thrive on pushing my limits
+          and embracing new challenges, both in my career and personal life.
         </p>
       </div>
 
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -20,38 +20,14 @@ import clsx from "clsx";
 import { siteConfig } from "@/config/site";
 import { ThemeSwitch } from "@/components/theme-switch";
 import {
-  TwitterIcon,
   LinkedInIcon,
   GithubIcon,
   DiscordIcon,
-  HeartFilledIcon,
-  SearchIcon,
   Logo,
   TelegramIcon,
 } from "@/components/icons";
 
 export const Navbar = () => {
-  const searchInput = (
-    <Input
-      aria-label="Search"
-      classNames={{
-        inputWrapper: "bg-default-100",
-        input: "text-sm",
-      }}
-      endContent={
-        <Kbd className="hidden lg:inline-block" keys={["command"]}>
-          K
-        </Kbd>
-      }
-      labelPlacement="outside"
-      placeholder="Search..."
-      startContent={
-        <SearchIcon className="text-base text-default-400 pointer-events-none flex-shrink-0" />
-      }
-      type="search"
-    />
-  );
-
   return (
     <NextUINavbar maxWidth="xl" position="sticky">
       <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
@@ -99,13 +75,12 @@ export const Navbar = () => {
           </Link>
           <ThemeSwitch />
         </NavbarItem>
-        <NavbarItem className="hidden lg:flex">{searchInput}</NavbarItem>
         <NavbarItem className="hidden md:flex">
           <Button
-            isExternal={true}
             as={Link}
             className="text-sm font-normal text-default-600 bg-default-100"
             href="https://t.me/mrnmfz"
+            isExternal={true}
             startContent={<TelegramIcon className="text-danger" />}
             variant="flat"
           >
@@ -123,7 +98,6 @@ export const Navbar = () => {
       </NavbarContent>
 
       <NavbarMenu>
-        {searchInput}
         <div className="mx-4 mt-2 flex flex-col gap-2">
           {siteConfig.navMenuItems.map((item, index) => (
             <NavbarMenuItem key={`${item}-${index}`}>
